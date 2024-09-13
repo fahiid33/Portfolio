41ad403465611371828585d8150864951ca54f8e
fahiid33 <trey34809@gmail.com>
2024-09-13 18:26:06 +0100

    fix hosting

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: '', // If using a base path, specify it here
-  assetPrefix: '', // If deploying to a subdirectory, specify it here
+  basePath: '', 
+  assetPrefix: '', 
   webpack: (config) => {
     config.module.rules.push({
       test: /\.pdf$/,
